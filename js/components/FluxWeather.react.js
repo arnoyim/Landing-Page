26f1788939cc559b2ac88cb3f881566e429f2c08
arno yim <arno@justyo.co>
2015-11-25 20:25:11 -0800

    Cleaned.

--- js/components/FluxWeather.react.js
+++ js/components/FluxWeather.react.js
@@ -4,21 +4,23 @@ var React = require('react');
 var FluxWeather = React.createClass({
   render: function() {
     var city = this.props.weather;
-    var tempK;
-    var description;
+    var tempK = null;
+    var description= null;
     for (i in city.main){
       tempK = city.main["temp"];
     }
-    var tempF = Math.round((tempK - 273.15) * 1.80 + 32.00);
+    if (tempK != null) {
+      var tempF = Math.round((tempK - 273.15) * 1.80 + 32.00) + " F";
+    }
     for (i in city.weather) {
       description = city.weather[0]['description']
     }
     return (
         <div className="flux-weather">
             <h1 className="city">{city.name}</h1>
-            <p className="city-temp">{tempF} F</p>
+            <p className="city-temp">{tempF}</p>
             <p className="city-desc">{description}</p>
-        </div>
+        </div> || ""
     );
   },
 });
